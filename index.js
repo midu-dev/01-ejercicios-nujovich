02b81f23262574ea17b28b1d546b8e08817b8efe
nadiauj <nadiauj@ext.inditex.com>
2023-07-24 08:56:32 +0200

    Ejercicio 4 terminado

--- index.js
+++ index.js
@@ -6,40 +6,43 @@ const content = process.argv[3];
 
 // Ejercicio 2
 async function writeFile(filePath, content, callback) {
-  const file = path.join(__dirname, filePath);
-  fs.writeFile(file, content, (err) => {
+  const dir = path.dirname(filePath);
+  fs.mkdir(dir, { recursive: true }, (err) => {
     if (err) {
-      fs.mkdir(path.dirname(file), { recursive: true }, (err) => {
-        if (err) {
-          callback(err);
-          return;
-        }
-        console.log("Directorio creado con éxito");
-        fs.writeFile(file, content, (err) => {
-          if (err) {
-            callback(err);
-            return;
-          }
-          callback(null, "El contenido fue escrito con éxito");
-        });
-      });
-      callback(err);
+      callback(new Error("No se pudo crear el directorio"));
       return;
     }
-    callback(null, "El contenido fue escrito con éxito");
+    console.log("Directorio creado con éxito");
+    fs.writeFile(filePath, content, (err) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+      callback(null, "El contenido fue escrito con éxito");
+    });
   });
 }
 
 // Ejercicio 3
-async function readFileAndCount(word, callback) {}
-
-writeFile(filePath, content, (err, message) => {
-  if (err) {
-    console.error("Archivo no encontrado, se creará uno nuevo");
-  } else {
-    console.log(message);
+async function readFileAndCount(word, callback) {
+  const path = process.argv[2];
+  let count = 0;
+  if (!path) {
+    callback(new Error("No se ha especificado el path del archivo"));
   }
-});
+  if (!word) {
+    callback(new Error("No se ha especificado la palabra a buscar"));
+  }
+
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      callback(null, 0);
+      return;
+    }
+    count = data.toString().split(word).length - 1;
+    callback(null, count);
+  });
+}
 
 module.exports = {
   writeFile,
